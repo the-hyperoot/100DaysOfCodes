4cfdd253369e1843b65a8fa7a5401c116e6e4ae1
PyrokineticRaj <rajesh.kumar.das.tech@gmail.com>
2022-10-09 12:10:42 +0530

    first commit

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,5 @@
+
+print("Not sure what should be your band name? Well, let me generate one for you!")
+a = input("Which city did you grow up in?")
+b = input("What's the name of your pet?")
+print("An interesting band name for you would be "+a+" "+b)
